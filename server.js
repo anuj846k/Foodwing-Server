fd2972b54ddd2db34e27792263d70a307d16ba7e
Anuj Kumar <anuj846k@gmail.com>
2024-06-12 23:51:05 +0530

    .

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/api/restaurants', async (req, res) => {
 
 // For Menu API
 app.get('/api/menu', async (req, res) => {
-    const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng, submitAction, restaurantId } = req.query;
+    const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng , restaurantId } = req.query;
     console.log(req.query);
 
     const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}`;
