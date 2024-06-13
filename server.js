3a16aa4f30e0aedcd3c5819509a73376a180b327
Anuj Kumar <anuj846k@gmail.com>
2024-06-13 21:51:05 +0530

    ,,,

--- server.js
+++ server.js
@@ -14,63 +14,56 @@ app.get('/api/restaurants', async (req, res) => {
 
     const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=${page_type}`;
 
-    await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-
-        }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+    try {
+        const response = await fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
             }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            res.json(data);
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).send('An error occurred');
         });
-});
 
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching restaurant data:', error);
+        res.status(500).send('An error occurred while fetching restaurant data');
+    }
+});
 
 // For Menu API
 app.get('/api/menu', async (req, res) => {
-    const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng , restaurantId } = req.query;
+    const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng, restaurantId } = req.query;
     console.log(req.query);
 
     const url = `https://www.swiggy.com/dapi/menu?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}`;
 
-
-    await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-
-        }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+    try {
+        const response = await fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
             }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            res.json(data);
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).send('An error occurred');
         });
-});
 
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching menu data:', error);
+        res.status(500).send('An error occurred while fetching menu data');
+    }
+});
 
 app.get('/', (req, res) => {
     res.json({ "test": "Welcome to Foodwing! - See Live Web URL for this Server - https://foodwing.netlify.app/" });
